9bbc74f8a23b9fb972d0702b5811439418d7fbbd
Владислав Шотов <shotov.vlad@mail.ru>
2021-03-17 09:06:37 +0300

    Homework_4

--- Task_1/Task_1.cpp
+++ Task_1/Task_1.cpp
@@ -1,6 +1,118 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+//ЧИСЛО СУДЬБЫ. Цифры полной даты рождения суммируются, пока не получится одна цифра. Разработайте функцию и вычислите свое число судьбы)
+
+int enter(int number, string str);
+bool check(int y, int m, int d);
+int numberOfFate(int y, int m, int d);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int year = 0, month = 0, day = 0;
+
+    cout << "Enter your birthday: " << endl;
+    year = enter(year, "Year");
+    month = enter(month, "Month");
+    day = enter(day, "Day");
+
+    if (check(year, month, day) != true)
+    {
+        cout << "\nThere is no such date." << endl;
+
+        return -1;
+    }
+    else {}
+
+    cout << "\nNumber of fate: " << numberOfFate(year, month, day) << endl;
+
+    return 0;
+}
+
+int enter(int number, string str)
+{
+    cout << str << ": ";
+    cin >> number;
+
+    return number;
+}
+
+bool check(int y, int m, int d)
+{
+    if (y < 0)
+    {
+        return false;
+    }
+    else if (m < 1 or m > 12)
+    {
+        return false;
+    }
+    else if (m == 1 and (d < 1 or d > 31))
+    {
+        return false;
+    }
+    else if (m == 2 and (d < 1 or d > 28 + (((y % 4) == 0) && (((y % 100) != 0) || (y % 400)) ? 1 : 0)))
+    {
+        return false;
+    }
+    else if (m == 3 and (d < 1 or d > 31))
+    {
+        return false;
+    }
+    else if (m == 4 and (d < 1 or d > 30))
+    {
+        return false;
+    }
+    else if (m == 5 and (d < 1 or d > 31))
+    {
+        return false;
+    }
+    else if (m == 6 and (d < 1 or d > 30))
+    {
+        return false;
+    }
+    else if (m == 7 and (d < 1 or d > 31))
+    {
+        return false;
+    }
+    else if (m == 8 and (d < 1 or d > 31))
+    {
+        return false;
+    }
+    else if (m == 9 and (d < 1 or d > 30))
+    {
+        return false;
+    }
+    else if (m == 10 and (d < 1 or d > 31))
+    {
+        return false;
+    }
+    else if (m == 11 and (d < 1 or d > 30))
+    {
+        return false;
+    }
+    else if (m == 12 and (d < 1 or d > 31))
+    {
+        return false;
+    }
+    else return true;
+}
+
+int numberOfFate(int y, int m, int d)
+{
+    int number = 0, sum = 0;
+
+    number = d + m * 100 + y * 10000;
+
+    while (number > 0)
+    {
+        sum += number % 10;
+
+        sum = sum % 10 + sum / 10;
+
+        number /= 10;
+    }
+
+    return sum;
 }
--- Task_2/Task_2.cpp
+++ Task_2/Task_2.cpp
@@ -1,6 +1,66 @@
 ﻿#include <iostream>
 
+using namespace std;
+
+//РАЗРЯДЫ. Разработайте функцию, которая "переворачивает" переданное ей целое число, например: 1234 -> 4321
+
+int enter(int number);
+int flipPositive(int number);
+int flipNegative(int number);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int number = 0, inverted = 0;
+
+
+    number = enter(number);
+
+    if (number >= 0)
+    {
+        inverted = flipPositive(number);
+    }
+    else
+    {
+        inverted = flipNegative(number);
+    }
+
+    cout << "\nInverted number: " << inverted << endl;
+
+    return 0;
+}
+
+int enter(int number)
+{
+    cout << "Enter number: ";
+    cin >> number;
+
+    return number;
+}
+
+int flipPositive(int number)
+{
+    int flip = 0;
+
+    while (number > 0)
+    {
+        flip = (flip * 10) + (number % 10);
+
+        number /= 10;
+    }
+
+    return flip;
+}
+
+int flipNegative(int number)
+{
+    int flip = 0;
+
+    while (number < 0)
+    {
+        flip = (flip * 10) + (number % 10);
+
+        number /= 10;
+    }
+
+    return flip;
 }
